cbf5cb866932eacb09edb63344124744e49b27ab
Rafael Sampaio Tavares <rafael_killua@msn.com>
2019-09-28 16:24:00 -0300

    clear resolvers and services

--- services/MongoDBService.js
+++ services/MongoDBService.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
-try {
-  mongoose.connect(process.env.MONGODB_URI, () => {
-  })
-  mongoose.Error.messages.general.required = "The attribute '{PATH}' is required"
-  mongoose.Error.messages.Number.min = "The '{VALUE}' reported is less than the minimun threshold of '{MIN}'"
-  mongoose.Error.messages.Number.max = "The '{VALUE}' reported is greater than the maximun limit of '{MAX}'"
-  mongoose.Error.messages.String.enum = "'{VALUE}' is not valid for attribute '{PATH}'"
-} catch (err) {
-  throw err
-}
+mongoose.connect(process.env.MONGODB_URI, () => { console.log('Online') })
+mongoose.Error.messages.general.required = "The attribute '{PATH}' is required"
+mongoose.Error.messages.Number.min = "The '{VALUE}' reported is less than the minimun threshold of '{MIN}'"
+mongoose.Error.messages.Number.max = "The '{VALUE}' reported is greater than the maximun limit of '{MAX}'"
+mongoose.Error.messages.String.enum = "'{VALUE}' is not valid for attribute '{PATH}'"
